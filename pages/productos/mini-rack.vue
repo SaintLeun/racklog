a574a57943e22947721402dbc61e7c3a065748d8
dalmuna <dalmuna@mac-dalmuna.bch.bandodechile.cl>
2025-03-08 11:29:26 -0300

    Feature | Se agregan page productos al catalogo y se modifca redirección a los mismo

---
+++ pages/productos/mini-rack.vue
@@ -0,0 +1,87 @@
+<template>
+  <section class="text-gray-600 body-font overflow-hidden">
+    <div class="container p-5 mx-auto">
+      <Breadcrumbs :breadcrumbs="[{ label: 'Rack Selectivo' }]" />
+    </div>
+  </section>
+  <section class="text-gray-600 body-font overflow-hidden">
+    <div class="container px-5 py-10 mx-auto">
+      <div class="lg:w-4/5 mx-auto flex flex-wrap">
+        <div class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded">
+          <iframe title="Modelo_RS_Ejemplo" class="w-full h-full" frameborder="0" allowfullscreen
+            mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking"
+            xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share
+            src="https://sketchfab.com/models/1d90e83ff7704eaa9d7b0cc5f0f6a804/embed">
+          </iframe>
+        </div>
+        <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+          <h2 class="text-sm title-font text-gray-500 tracking-widest uppercase">Estanterías Industriales para Picking
+            Eficiente</h2>
+          <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">MINI RACK</h1>
+
+          <p class="leading-relaxed text-justify pt-3">Las estanterías Mini Rack de Racklog son la solución ideal para
+            el almacenamiento de cargas medias, ubicándose entre las estanterías manuales y los racks para pallets. Este
+            sistema optimiza las operaciones de picking, ofreciendo un equilibrio perfecto entre capacidad de carga y
+            acceso directo. Diseñadas para una variedad de industrias, nuestras estanterías Mini Rack mejoran la
+            eficiencia y organización de tu almacén.
+          </p>
+          <h3 class="text-2xl text-black py-3">Ventajas del Rack Selectivo</h3>
+          <ul class="list-disc pl-10">
+            <li><b>Picking Eficiente:</b> Acceso manual rápido a cada nivel, mejorando la preparación de pedidos.</li>
+            <li><b>Capacidad de Carga Versátil:</b> Soportan desde 250 kg hasta 1000 kg por nivel, adaptándose a
+              diferentes necesidades.</li>
+            <li><b>Diseño adaptable:</b> Configuración ajustable con opciones de planos de carga metálicos o de madera.
+            </li>
+            <li><b>Optimización del Espacio:</b> Mayor espaciamiento entre pilares para productos de mayor volumen y
+              peso.</li>
+            <li><b>Selectividad directa:</b> Acceso inmediato a cada producto, facilitando la gestión de inventario.
+            </li>
+            <li><b>Control de Inventario Mejorado:</b> Organización y visibilidad claras de los productos almacenados.
+            </li>
+          </ul>
+          <div class="mt-10">
+            <button
+              class="flex w-full bg-orange-400 hover:bg-orange-500 text-white border-0 py-2 px-6 focus:outline-none rounded mt-2 transition-colors">Solicitar
+              Cotización</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="text-gray-600 body-font">
+    <div class="container px-5 py-24 mx-auto">
+      <div class="flex flex-wrap -m-4">
+        <div class="p-4 md:w-1/2 w-full">
+          <div class="h-full bg-gray-100 text-gray-700 text-lg p-8 rounded">
+            <h3 class="text-2xl mb-4">¿Cómo funciona?</h3>
+            <p class="leading-relaxed mb-6">Las estanterías Mini Rack están diseñadas para operaciones de picking
+              eficientes. Su estructura permite un mayor espaciamiento entre pilares, aumentando el área de carga para
+              productos voluminosos. Los operadores acceden manualmente a los niveles, eliminando la necesidad de
+              maquinaria. Este sistema asegura un flujo de trabajo ágil y organizado.</p>
+          </div>
+        </div>
+        <div class="p-4 md:w-1/2 w-full">
+          <div class="h-full bg-gray-100 text-gray-700 text-lg p-8 rounded">
+            <h3 class="text-2xl mb-4">Usos</h3>
+            <p class="leading-relaxed mb-6">Nuestras estanterías Mini Rack son ideales para: </p>
+            <ul class="list-disc pl-10">
+              <li>Tiendas de ropa y calzado</li>
+              <li>Almacenes de productos farmacéuticos y repuestos</li>
+              <li>Centros de distribución de comercio electrónico</li>
+              <li>Empresas de minería y otros sectores industriales</li>
+              <li>Protecciones de pilares (canilleras)</li>
+              <li>Planos de carga para picking en los niveles mas bajos</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
---
+++ pages/productos/rack-autoportante.vue
@@ -0,0 +1,83 @@
+<template>
+  <section class="text-gray-600 body-font overflow-hidden">
+    <div class="container p-5 mx-auto">
+      <Breadcrumbs :breadcrumbs="[{ label: 'Rack Selectivo' }]" />
+    </div>
+  </section>
+  <section class="text-gray-600 body-font overflow-hidden">
+    <div class="container px-5 py-10 mx-auto">
+      <div class="lg:w-4/5 mx-auto flex flex-wrap">
+        <div class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded">
+          <iframe title="Modelo_RS_Ejemplo" class="w-full h-full" frameborder="0" allowfullscreen
+            mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking"
+            xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share
+            src="https://sketchfab.com/models/1d90e83ff7704eaa9d7b0cc5f0f6a804/embed">
+          </iframe>
+        </div>
+        <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+          <h2 class="text-sm title-font text-gray-500 tracking-widest">ESTRUCTURA Y ALMACENAMIENTO: TODO EN UNO</h2>
+          <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">RACK AUTOPORTANTE</h1>
+
+          <p class="leading-relaxed text-justify pt-3">El <b>Rack Autoportante</b> de Racklog es una solución avanzada
+            que
+            integra el almacenamiento con la estructura de la bodega. Este sistema innovador sirve como <b>estantería
+              industrial</b> y edificación, optimizando cada área del almacén. Diseñado para cargas convencionales y
+            <b>almacenes
+              automatizados</b>, el Rack Autoportante maximiza el espacio y reduce costos de construcción.
+          </p>
+          <h3 class="text-2xl text-black py-3">Ventajas del Rack Autoportante</h3>
+          <ul class="list-disc pl-10">
+            <li><b>Optimización Máxima del Espacio:</b> Integración total con la estructura del edificio.</li>
+            <li><b>Versatilidad de Carga:</b> Adecuado para todo tipo de estanterías de carga paletizada.</li>
+            <li><b>Alta Capacidad de Carga:</b> Soportes robustos para cargas pesadas.</li>
+            <li><b>Construcción Eficiente: </b> Solución integral en una sola fase, ahorrando tiempo y costos.</li>
+            <li><b>Reducción de Costos:</b> Menor costo de implementación al integrar estructura y almacenamiento.</li>
+            <li><b>Automatización Flexible:</b> Factible para sistemas de carga convencionales o almacenes
+              automatizados.</li>
+            <li><b>Construcción Rápida:</b> Montaje ágil y eficiente.</li>
+          </ul>
+          <div class="mt-10">
+            <button
+              class="flex w-full bg-orange-400 hover:bg-orange-500 text-white border-0 py-2 px-6 focus:outline-none rounded mt-2 transition-colors">Solicitar
+              Cotización</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="text-gray-600 body-font">
+    <div class="container px-5 py-24 mx-auto">
+      <div class="flex flex-wrap -m-4">
+        <div class="p-4 md:w-1/2 w-full">
+          <div class="h-full bg-gray-100 text-gray-700 text-lg p-8 rounded">
+            <h3 class="text-2xl mb-4">¿Cómo funciona?</h3>
+            <p class="leading-relaxed mb-6">El Rack Autoportante funciona como un sistema estructural completo, donde
+              las estanterías industriales soportan tanto la carga almacenada como los elementos estructurales del
+              edificio. Elimina la necesidad de construir una estructura independiente, optimizando costos y tiempos de
+              construcción.</p>
+          </div>
+        </div>
+        <div class="p-4 md:w-1/2 w-full">
+          <div class="h-full bg-gray-100 text-gray-700 text-lg p-8 rounded">
+            <h3 class="text-2xl mb-4">Usos</h3>
+            <p class="leading-relaxed mb-6">El Rack Autoportante de Racklog es ideal para: </p>
+            <ul class="list-disc pl-10">
+              <li>Almacenes de alta densidad.</li>
+              <li>Centros de distribución automatizados.</li>
+              <li>Bodegas con limitaciones de espacio</li>
+              <li>Proyectos que requieren construcción rápida y eficiente.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
