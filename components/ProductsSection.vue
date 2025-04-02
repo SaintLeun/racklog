c375b0481492c9f3f066965a98bbf6b1416e182d
dalmuna <dalmuna@mac-dalmuna.bch.bandodechile.cl>
2025-04-01 21:25:58 -0300

    Upgrade productos en products.json

--- components/ProductsSection.vue
+++ components/ProductsSection.vue
@@ -16,7 +16,7 @@
                 </div>
                 <div class="flex-grow">
                   <p class="leading-relaxed text-base">{{ product.description.short }}</p> 
-                  <a :href="'/prod/' + product.slug" class="mt-3 text-indigo-500 inline-flex items-center">Ver más
+                  <a :href="'/productos/' + product.slug" class="mt-3 text-indigo-500 inline-flex items-center">Ver más
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                       <path d="M5 12h14M12 5l7 7-7 7"></path>
                     </svg>
